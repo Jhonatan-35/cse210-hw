1b774fbe32896f914352f9a5fcd040f2e10acbee
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-01-14 18:09:54 -0500

    learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Civil engineer";
+        job1._company = "  Gft";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Programmer";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jhonatan Yugsan";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
